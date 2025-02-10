a69f15ffb18f4f51fa36590834c136a84f3c21ff
Heclgg <heclgg@foxmail.com>
2025-02-10 13:44:30 +0800

    init project

--- OpenCV/build/intermediates/cxx/Debug/1m4l4t3o/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\heclg\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Code\\eggCut\\OpenCV\\.cxx\\Debug\\1m4l4t3o\\armeabi-v7a" ^
-  opencv_jni_shared

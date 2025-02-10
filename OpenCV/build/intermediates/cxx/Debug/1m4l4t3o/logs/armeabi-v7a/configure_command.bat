320498c676e278002e92027801edced17f5201eb
Heclgg <heclgg@foxmail.com>
2025-02-10 13:37:40 +0800

    init project

---
+++ OpenCV/build/intermediates/cxx/Debug/1m4l4t3o/logs/armeabi-v7a/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\heclg\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\Code\\eggCut\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=armeabi-v7a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a" ^
+  "-DANDROID_NDK=C:\\Users\\heclg\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\heclg\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\heclg\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\heclg\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\Code\\eggCut\\OpenCV\\build\\intermediates\\cxx\\Debug\\1m4l4t3o\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\Code\\eggCut\\OpenCV\\build\\intermediates\\cxx\\Debug\\1m4l4t3o\\obj\\armeabi-v7a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BD:\\Code\\eggCut\\OpenCV\\.cxx\\Debug\\1m4l4t3o\\armeabi-v7a" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
---
+++ OpenCV/build/intermediates/cxx/Debug/1m4l4t3o/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\heclg\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Code\\eggCut\\OpenCV\\.cxx\\Debug\\1m4l4t3o\\x86" ^
+  opencv_jni_shared
